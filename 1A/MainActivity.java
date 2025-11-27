ee1d7a7d7676da96368eaf4ed94927ede05d4b58
Ramith.s <86591506+rohit7388@users.noreply.github.com>
2025-11-27 11:20:17 +0530

    Update MainActivity.java

--- 1A/MainActivity.java
+++ 1A/MainActivity.java
@@ -1,14 +1,3 @@
-package com.example.a2;
-
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.Toast;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override

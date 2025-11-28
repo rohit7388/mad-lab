88a828dc66ce528b5fa7c1efe66672eeb041ce47
Ramith.s <86591506+rohit7388@users.noreply.github.com>
2025-11-28 20:37:37 +0530

    Update MainActivity.java

--- 7A/MainActivity.java
+++ 7A/MainActivity.java
@@ -1,4 +1,3 @@
-package com.example.a7;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -10,6 +9,9 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,24 +20,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         EditText num= findViewById(R.id.et);
         Button call= findViewById(R.id.button);
         call.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 String number=num.getText().toString();
                 Intent callintent=new Intent(Intent.ACTION_CALL);
                 callintent.setData(Uri.parse("tel:"+number));
-                try {
-                    startActivity(callintent);
+                try {startActivity(callintent);
                 }catch (Exception e){
                     Log.d("error",e.getMessage());
                     Log.d("error",e.getMessage());
-                    Toast.makeText(getApplicationContext(),"call permission denied",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(),"call permission denied",Toast.LENGTH_SHORT).show();}}
 
-                }
-            }
+
         });
     }
 }

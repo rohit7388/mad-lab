88d9de8a18e209f53d6415f28e55dcc137f5c3cf
Ramith.s <86591506+rohit7388@users.noreply.github.com>
2025-11-28 21:12:56 +0530

    Update MainActivity.java

--- 9A/MainActivity.java
+++ 9A/MainActivity.java
@@ -1,9 +1,3 @@
-package com.example.a9;
-
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
@@ -11,38 +5,36 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         Button bt=findViewById(R.id.button);
-        //imageView = findViewById(R.id.imageView); 
+        //imageView = findViewById(R.id.imageView);
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(i,1);
-            }
-        });
-
-    }
-
+                startActivityForResult(i,1);}});}
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if(requestCode==1 && resultCode==RESULT_OK)
-        {
-
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        if(requestCode==1 && resultCode==RESULT_OK) {
             Bitmap image = (Bitmap) data.getExtras().get("data");
-            //imageView.setImageBitmap(image); 
+            //imageView.setImageBitmap(image);
             BitmapDrawable drawable= new BitmapDrawable(image);
             ConstraintLayout r=findViewById(R.id.cl);
-            r.setBackground(drawable);
-        }
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-} 
- 
+            r.setBackground(drawable);}
+        super.onActivityResult(requestCode, resultCode, data);}}

2097507deeba5329f07d4f8eee6f6055ed2eefa4
Ramith.s <86591506+rohit7388@users.noreply.github.com>
2025-11-28 20:14:19 +0530

    Update MainActivity.java

--- 5A/MainActivity.java
+++ 5A/MainActivity.java
@@ -1,7 +1,4 @@
-package com.example.a4;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
+package com.example.contextmenu;
 
 import android.graphics.Color;
 import android.os.Bundle;
@@ -11,37 +8,38 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        TextView t1 =  findViewById(R.id.textview);
-        registerForContextMenu(t1);
-    }
 
+
+        TextView t1 =  findViewById(R.id.textview);
+        registerForContextMenu(t1);}
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v,
                                     ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.m,menu);
-
-    }
-
+        inflater.inflate(R.menu.m,menu);}
     @Override
     public boolean onContextItemSelected(@NonNull MenuItem item) {
         ConstraintLayout cl = findViewById(R.id.cl);
         if (item.getItemId() == R.id.red) {
             cl.setBackgroundColor(Color.RED);
         } else if (item.getItemId() == R.id.blue) {
-            cl.setBackgroundColor(Color.BLUE);
-        } else if (item.getItemId() == R.id.green) {
-            cl.setBackgroundColor(Color.GREEN);
-        }
-        return super.onContextItemSelected(item);
-
-    }
-
-}
+            cl.setBackgroundColor(Color.BLUE);}
+        else if (item.getItemId() == R.id.green) {
+            cl.setBackgroundColor(Color.GREEN);}
+        return super.onContextItemSelected(item);}}

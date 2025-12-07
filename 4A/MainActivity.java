f3b52420c97d103af3bcce8a95127e29fe8541ba
Ramith.s <86591506+rohit7388@users.noreply.github.com>
2025-12-08 00:11:54 +0530

    Update MainActivity.java

--- 4A/MainActivity.java
+++ 4A/MainActivity.java
@@ -1,24 +1,4 @@
-package com.example.a3;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-
-import androidx.appcompat.widget.Toolbar;
-
-import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
 
